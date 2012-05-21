fae01a8d6df5205a218e3ecc874369bd47d00e15
Iain Barnett <iainspeed@gmail.com>
2012-05-21 17:59:33 +0100

    Added simplecov and some branching for 1.8, and removed the requires for the lib, the individual spec files will deal with that.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,24 @@
 # encoding: UTF-8
 
 require "rspec"
-Spec_dir = File.expand_path( File.dirname __FILE__ )
-require_relative '../lib/cicero/cicero_text.rb'
 
-require_relative '../lib/cicero.rb' # and any other gems you need
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
+# code coverage
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/vendor/"
+end
+
+
+Spec_dir = File.expand_path( File.dirname __FILE__ )
+
 
 Dir[ File.join( Spec_dir, "/support/**/*.rb")].each do |f| 
   puts "requiring #{f}"
